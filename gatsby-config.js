21895badfc8eb4d0d43a4e00f7be1bc96a03c11f
agis3636 <asyelom@gmail.com>
2023-07-21 19:39:57 +0700

    adfasdfasf

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Arch Tech",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Belajar IT tanpa mikir",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
